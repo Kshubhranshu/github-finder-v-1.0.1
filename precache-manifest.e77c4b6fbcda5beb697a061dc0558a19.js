6e56900eb963c4b0b0a1978cac0f4d21a0e1f035
kshubhranshu <shubhranshu14@gmail.com>
2020-04-28 12:09:03 +0530

    Updates

---
+++ precache-manifest.e77c4b6fbcda5beb697a061dc0558a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee57a1caf5e2f2ff5b01baa5b6039244",
+    "url": "/github-finder-v-1.0.1/index.html"
+  },
+  {
+    "revision": "47daa37687c700e33348",
+    "url": "/github-finder-v-1.0.1/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "94d2fcf4da59cb8c9373",
+    "url": "/github-finder-v-1.0.1/static/js/2.3edaf50f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder-v-1.0.1/static/js/2.3edaf50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47daa37687c700e33348",
+    "url": "/github-finder-v-1.0.1/static/js/main.148dbed8.chunk.js"
+  },
+  {
+    "revision": "8e02ee57522d2e28e66b",
+    "url": "/github-finder-v-1.0.1/static/js/runtime-main.e0bd9698.js"
+  }
+]);
